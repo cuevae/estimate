1d2ab41dac84e3a6eea71e820edec567b87ca4ed
manei <manei_cc@hotmail.com>
2014-06-24 23:58:59 +0100

    composer and index file

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require '../vendor/autoload.php';
+
+const ENVIRONMENT = 'development';
+const SLIM_CONFIG_PATH = 'config/slim/app-config.json';
+
+$configLoader = json_decode( file_get_contents( SLIM_CONFIG_PATH ), true );
+$appConfig = $configLoader[ENVIRONMENT];
+
+$app = new \Slim\Slim( $appConfig );
+
+$app->get( '/hello/:name', function ( $name ) {
+    echo "Hello, $name";
+} );
+
+$app->run();
