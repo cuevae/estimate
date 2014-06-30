30120e21c99cf210f90de841291a9f94c3878ef0
manei <manei_cc@hotmail.com>
2014-07-01 00:20:07 +0100

    updates and committing some of the tests

---
+++ lib/collectionPlusJson/tests/AnonymousObjectTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manei
+ * Date: 30/06/14
+ * Time: 22:54
+ */
+
+class AnonymousObjectTest {
+
+} 
---
+++ lib/collectionPlusJson/tests/HrefTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manei
+ * Date: 30/06/14
+ * Time: 23:31
+ */
+
+class HrefTest {
+
+} 
