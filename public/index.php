df2ca4bbb1ae98094455addc00995465e64b9803
manei <manei_cc@hotmail.com>
2014-07-09 20:09:16 +0100

    updated index file to use collectionPlusJson library

--- public/index.php
+++ public/index.php
@@ -4,88 +4,66 @@ require '../vendor/autoload.php';
 
 use \CollectionPlusJson\Collection;
 use \CollectionPlusJson\Item;
-use \CollectionPlusJson\Data;
-use \CollectionPlusJson\Data\Href;
-use \CollectionPlusJson\Data\AnonymousObject;
+use \CollectionPlusJson\Util\Href;
+use \CollectionPlusJson\DataObject;
 
 const ENVIRONMENT = 'development';
 const SLIM_CONFIG_PATH = '../config/slim/app-config.json';
 
-$configLoader = json_decode( file_get_contents( SLIM_CONFIG_PATH ), true );
+$configLoader = json_decode(file_get_contents(SLIM_CONFIG_PATH), true);
 $appConfig = $configLoader[ENVIRONMENT];
 
-$app = new \Slim\Slim( $appConfig );
+$app = new \Slim\Slim($appConfig);
 
-$app->get( '/project/:projectId', function ( $projectId ) use ( $app ) {
+$app->get(
+    '/project/:projectId',
+        function ($projectId) use ($app) {
 
 
-} );
+        }
+);
 
-$app->get( '/', function () use ( $app ) {
+$app->get(
+    '/',
+        function () use ($app) {
 
-    $app->response->headers->set( 'Content-Type', 'application/vnd.collection+json' );
+            $app->response->headers->set('Content-Type', 'application/vnd.collection+json');
 
-    $project1 =
-        new Item(
-            new Href( 'http://api.estimate.local/project/1' ),
-            new Data(
-                new AnonymousObject(
-                    'name',
-                    'Project Test',
-                    'This is the Project Name'
-                ),
-                new AnonymousObject(
-                    'due_date_ts',
-                    strtotime( '+1month' ),
-                    'This is the project due date specified in UNIX timestamp, seconds since epoch'
-                ),
-                new AnonymousObject(
-                    'due_date_human',
-                    date( 'Y-m-d H:i:s', strtotime( '+1month' ) ),
-                    'This is the project due date in human readable format'
-                )
-            )
-        );
+            $project1 = new Item(new Href('http://api.estimate.local/project/1'));
+            $project1->addData(
+                     new DataObject('name',
+                                    'Project Test',
+                                    'This is the Project Name'))
+                     ->addData(
+                     new DataObject('due_date_ts',
+                                    strtotime('+1month'),
+                                    'This is the project due date specified in UNIX timestamp, seconds since epoch'))
+                     ->addData(
+                     new DataObject('due_date_human',
+                                    date('Y-m-d H:i:s', strtotime('+1month')),
+                                    'This is the project due date in human readable format'));
 
-    $project2 =
-        new Item(
-            new Href( 'http://api.estimate.local/project/2' ),
-            new Data(
-                new AnonymousObject(
-                    'name',
-                    'Wololo',
-                    'This is the Project Name'
-                ),
-                new AnonymousObject(
-                    'due_date_ts',
-                    strtotime( '+1week' ),
-                    'This is the project due date specified in UNIX timestamp, seconds since epoch'
-                ),
-                new AnonymousObject(
-                    'due_date_human',
-                    date( 'Y-m-d H:i:s', strtotime( '+1week' ) ),
-                    'This is the project due date in human readable format'
-                )
-            )
-        );
+            $project2 = new Item(new Href('http://api.estimate.local/project/2'));
+            $project2
+                ->addData(new DataObject('name',
+                                         'Wololo',
+                                         'This is the Project Name'))
+                ->addData(new DataObject('due_date_ts',
+                                         strtotime('+1week'),
+                                         'This is the project due date specified in UNIX timestamp, seconds since epoch'))
+                ->addData(new DataObject('due_date_human',
+                                         date('Y-m-d H:i:s', strtotime('+1week')),
+                                         'This is the project due date in human readable format'));
 
-    $collection = new Collection();
-    $collection->version = '0.0.1';
-    $collection->href = '/';
-    $collection->links = array();
-    $collection->items = array( $project1, $project2 );
-    $collection->queries = array();
-    $collection->template = new StdClass();
-    $collection->error = new StdClass();
+            $collection = new Collection('0.0.1', new Href('/'));
+            $collection->addItem($project1)->addItem($project2);
 
-    $template = new StdClass();
-    $template->data = array();
+            $response = new StdClass();
+            $response->collection = $collection->_output();
 
-    $response = new StdClass();
-    $response->collection = $collection->_output();
+            echo json_encode($response);
 
-    echo json_encode( $response );
-
-} );
+        }
+);
 
 $app->run();
